70427bae88b75e433bc8b78448c500f358a8ba31
jbrendel <juergen@brendel.com>
2010-08-20 09:33:23 +1200

    Enabled text/plain output rendering.

--- src/python/restx/render/textrenderer.py
+++ src/python/restx/render/textrenderer.py
@@ -27,7 +27,29 @@ class TextRenderer(BaseRenderer):
     """
 
     CONTENT_TYPE = "text/plain; charset=UTF-8"
-    CAN_RENDER = False
+
+    def render(self, data, top_level=False):
+        """
+        Render the provided data for output.
+        
+        @param data:        An object containing the data to be rendered.
+        @param data:        object
+        
+        @param top_level:   Flag indicating whether this we are at the
+                            top level for output (this function is called
+                            recursively and therefore may not always find
+                            itself at the top level). This is important for
+                            some renderers, since they can insert any framing
+                            elements that might be required at the top level.
+                            However, for the JSON renderer this is just
+                            ignored.
+        @param top_level:   boolean
+        
+        @return:            Output buffer with completed representation.
+        @rtype:             string
+        
+        """
+        return str(data)
 
     def parse(self, data):
         """
